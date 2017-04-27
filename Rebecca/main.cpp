308399d8c3f145499d1d35dbfd21bf32fe7aee78
meriy100 <ttattataa@gmail.com>
2017-04-27 15:21:52 +0900

    Initial Commit

---
+++ Rebecca/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Rebecca
+//
+//  Created by 苅宿航太 on 2017/04/27.
+//  Copyright © 2017年 meriy100. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
